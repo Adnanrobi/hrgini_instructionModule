d74607fa5ba1c6a6895a7c7dce0a16239f7a938e
Md. Adnan Rahman Robe <adnanrobi6@gmail.com>
2022-08-16 16:31:30 +0600

    Snapshot Interval added

--- src/Components/CameraAccess/CameraAccess1.js
+++ src/Components/CameraAccess/CameraAccess1.js
@@ -1,46 +1,48 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import Webcam from "react-webcam";
 import "./CameraAccess.css";
 
+let i = 0;
 const videoConstraints = {
-  width: 1280,
-  height: 720,
-  facingMode: "user",
-}
+width: 1280,
+height: 720,
+facingMode: "user",
+};
 const App = () => {
-  let images = []
-  const [length, setLength] = useState(0)
-  const webcamRef = React.useRef(null)
-  const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot()
-    images.push(imageSrc)
-    setLength(images.length)
-    console.log(images)
-    // eslint-disable-next-line
-  }, [webcamRef])
+let images = [];
+const [length, setLength] = useState(0);
+const webcamRef = React.useRef(null);
+const capture = React.useCallback(() => {
+const imageSrc = webcamRef.current.getScreenshot();
+images.push(imageSrc);
+setLength(images.length);
+i++;
+console.log(images);
+// eslint-disable-next-line
+}, [webcamRef]);
 
-  const submit = () => {
-    console.log("submitted")
-  }
+setInterval(() => {
+if (i < 5) capture();
+}, 5000);
 
-  return (
-    <>
-      <Webcam
-        audio={false}
-        height={720}
-        ref={webcamRef}
-        screenshotFormat='image/jpeg'
-        width={1280}
-        videoConstraints={videoConstraints}
-      />
-      
-      {length < 5 ? (
-        <button onClick={capture}>Capture photo</button>
-      ) : (
-        <button onClick={submit}>Submit</button>
-      )}
-    </>
-  )
-}
+return (
+<>
+<Webcam
+audio={false}
+height={720}
+ref={webcamRef}
+screenshotFormat="image/jpeg"
+width={1280}
+videoConstraints={videoConstraints}
+/>
 
-export default App
+{/* {length < 5 ? (
+<button onClick={capture}>Capture photo</button>
+) : (
+<button onClick={submit}>Submit</button>
+)} */}
+</>
+);
+};
+
+export default App;
